ae13fd3e6f35e9fd603742ca2954b788d14282ef
Luka Bračun <luka@proxima.si>
2023-02-23 15:07:04 +0100

    update

--- bricks/initialize/hooks/post_gen.dart
+++ bricks/initialize/hooks/post_gen.dart
@@ -2,26 +2,36 @@ import 'dart:io';
 
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  // Splash screen
-  await Process.run('flutter', [
-    'pub',
-    'run',
-    'flutter_native_splash:create',
-  ]);
-
+Future<void> run(HookContext context) async {
   // Icons
+  final iconsProgress = context.logger.progress(
+    'Creating default app icons',
+  );
   await Process.run('flutter', [
     'pub',
     'global',
     'activate',
     'flutter_launcher_icons',
   ]);
-
   await Process.run('flutter', [
     'pub',
     'global',
     'run',
     'flutter_launcher_icons',
   ]);
+  iconsProgress.complete();
+
+  // Splash screen
+  final splashProgress = context.logger.progress(
+    'Creating default splash screen',
+  );
+  final result = await Process.run('flutter', [
+    'pub',
+    'run',
+    'flutter_native_splash:create',
+  ]);
+  if (result.exitCode != 0) {
+    return await run(context);
+  }
+  splashProgress.complete();
 }
