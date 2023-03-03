8704f82e13aa03c873a1b13995e33dcf7b721415
Luka Bračun <luka@proxima.si>
2023-03-03 14:49:54 +0100

    update

--- bricks/initialize/__brick__/lib/app/app.dart
+++ bricks/initialize/__brick__/lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:{{project_name.snakeCase()}}/app/router/app_router.dart';
 import 'package:{{project_name.snakeCase()}}/common/common.dart';
 import 'package:{{project_name.snakeCase()}}/l10n/l10n.dart';
@@ -17,6 +18,15 @@ class App extends StatelessWidget {
       supportedLocales: AppLocalizations.supportedLocales,
       routerDelegate: appRouter.delegate(),
       routeInformationParser: appRouter.defaultRouteParser(),
+      builder: (context, child) {
+        final overlayStyle = Theme.of(context).appBarTheme.systemOverlayStyle;
+
+        if (overlayStyle != null) {
+          SystemChrome.setSystemUIOverlayStyle(overlayStyle);
+        }
+
+        return child ?? const SizedBox();
+      },
     );
   }
 }
--- bricks/initialize/__brick__/lib/common/theme/theme.dart
+++ bricks/initialize/__brick__/lib/common/theme/theme.dart
@@ -4,11 +4,15 @@ import 'package:{{project_name.snakeCase()}}/common/common.dart';
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
+      brightness: Brightness.light,
       extensions: [
         Colors.light,
         Typography.typography,
       ],
       useMaterial3: true,
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
       colorScheme: ColorScheme.light(
         background: Colors.light.background,
       ),
@@ -17,11 +21,15 @@ class AppTheme {
 
   static ThemeData get dark {
     return ThemeData(
+      brightness: Brightness.dark,
       extensions: [
         Colors.dark,
         Typography.typography,
       ],
       useMaterial3: true,
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
       colorScheme: ColorScheme.dark(
         background: Colors.dark.background,
       ),
