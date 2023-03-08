af30808fa14fe7e5d604e06cfd65844c29ad3dd5
Luka Bračun <luka@proxima.si>
2023-03-08 14:17:31 +0100

    update

--- bricks/initialize/__brick__/lib/common/theme/theme.dart
+++ bricks/initialize/__brick__/lib/common/theme/theme.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Colors, Typography;
+import 'package:flutter/services.dart';
 import 'package:{{project_name.snakeCase()}}/common/common.dart';
 
 class AppTheme {
