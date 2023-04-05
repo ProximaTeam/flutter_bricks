36c636c4e79e53f05320f514b09830e9b26d2f2b
Luka Bračun <luka@proxima.si>
2023-04-05 11:07:03 +0200

    update

--- bricks/page/__brick__/lib/pages/{{{path}}}/{{#cubit}}cubit{{/cubit}}/{{page_name.snakeCase()}}_page_state.dart
+++ bricks/page/__brick__/lib/pages/{{{path}}}/{{#cubit}}cubit{{/cubit}}/{{page_name.snakeCase()}}_page_state.dart
@@ -1,6 +1,10 @@
 part of '{{page_name.snakeCase()}}_page_cubit.dart';
 
+// Run: flutter pub run build_runner build
+
 @freezed
 class {{page_name.pascalCase()}}PageState with _${{page_name.pascalCase()}}PageState {
-  const factory {{page_name.pascalCase()}}PageState.initial() = _Initial;
+  const factory {{page_name.pascalCase()}}PageState.initial({
+    @Default(false) bool isInitialized,
+  }) = _Initial;
 }
