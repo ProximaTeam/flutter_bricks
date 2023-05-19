c529aace1e8b2a17930b4fc7eb5b3324355577e0
Luka Bračun <luka@proxima.si>
2023-05-19 09:40:43 +0200

    update

--- bricks/page/__brick__/lib/pages/{{{path}}}/{{page_name.snakeCase()}}_page.dart
+++ bricks/page/__brick__/lib/pages/{{{path}}}/{{page_name.snakeCase()}}_page.dart
@@ -3,7 +3,6 @@ import 'package:{{project_name.snakeCase()}}/common/common.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-
 class {{page_name.pascalCase()}}Page extends StatelessWidget {
   const {{page_name.pascalCase()}}Page({super.key});
 
