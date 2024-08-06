e4195fe34b58585583bccae8efc1390550f12330
Luka Bračun <luka@proxima.si>
2024-08-06 06:42:04 +0200

    update

--- bricks/bloc/__brick__/lib/blocs/{{bloc_name.snakeCase()}}/{{bloc_name.snakeCase()}}_bloc.dart
+++ bricks/bloc/__brick__/lib/blocs/{{bloc_name.snakeCase()}}/{{bloc_name.snakeCase()}}_bloc.dart
@@ -6,8 +6,8 @@ part '{{bloc_name.snakeCase()}}_event.dart';
 part '{{bloc_name.snakeCase()}}_state.dart';
 
 class {{bloc_name.pascalCase()}}Bloc extends Bloc<{{bloc_name.pascalCase()}}Event, {{bloc_name.pascalCase()}}State> {
-  {{bloc_name.pascalCase()}}Bloc() : super({{bloc_name.pascalCase()}}State.initial()) {
-    on<{{bloc_name.pascalCase()}}Event>(_onUserEvent);
+  {{bloc_name.pascalCase()}}Bloc() : super(const {{bloc_name.pascalCase()}}State.initial()) {
+    on<{{bloc_name.pascalCase()}}Event>(_on{{bloc_name.pascalCase()}}Event);
   }
 
   void _on{{bloc_name.pascalCase()}}Event(
