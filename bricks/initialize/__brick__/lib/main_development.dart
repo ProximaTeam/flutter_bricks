1a5cb44da7e9891e6d7c89a0cf2315e29db56007
Luka Bračun <luka@proxima.si>
2025-02-28 09:14:16 +0100

    update

--- bricks/initialize/__brick__/lib/main_development.dart
+++ bricks/initialize/__brick__/lib/main_development.dart
@@ -1,11 +1,9 @@
-import 'package:{{project_name.snakeCase()}}/app/app.dart';
 import 'package:{{project_name.snakeCase()}}/bootstrap.dart';
+import 'package:{{project_name.snakeCase()}}/build_app.dart';
 import 'package:{{project_name.snakeCase()}}/common/common.dart';
 
 void main() {
   bootstrap(
-    () => App(
-      config: const AppConfig({{project_name.camelCase()}}ApiUrl: ''),
-    ),
+    () => buildApp(const AppConfig({{project_name.camelCase()}}ApiUrl: '')),
   );
 }
--- bricks/initialize/__brick__/lib/main_production.dart
+++ bricks/initialize/__brick__/lib/main_production.dart
@@ -1,13 +1,11 @@
 import 'package:talker/talker.dart';
-import 'package:{{project_name.snakeCase()}}/app/app.dart';
 import 'package:{{project_name.snakeCase()}}/bootstrap.dart';
+import 'package:{{project_name.snakeCase()}}/build_app.dart';
 import 'package:{{project_name.snakeCase()}}/common/common.dart';
 
 void main() {
   bootstrap(
-    () => App(
-      config: const AppConfig({{project_name.camelCase()}}ApiUrl: ''),
-    ),
+    () => buildApp(const AppConfig({{project_name.camelCase()}}ApiUrl: '')),
     talkerSettings: TalkerSettings(useConsoleLogs: false),
   );
 }
